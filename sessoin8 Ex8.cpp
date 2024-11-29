40c757c4d693009c758d779a39e1c291e5de4e2c
nguyenbabie1 <brokeniuem569@gmail.com>
2024-11-29 10:16:22 +0700

    Update sessoin8 Ex8.cpp

--- sessoin8 Ex8.cpp
+++ sessoin8 Ex8.cpp
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int arr[6][6]={
+	int arr[3][6]={
 	 {3,0,6,2,0,6},
 	 {9,0,8,2,0,8},
-	 {5,6,7,8,9,1},
-	{3,4,5,3,4,2},
-	
-	};
-	
+	 {5,6,7,8,9,1}
+};
 	int sum=0;
 	printf("Duong cheo phu la: ");
 	for(int i =0 ;i<5; i++)
